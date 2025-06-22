d43ed0566e99d7bc23d4462158a4424a00332b52
hsgamer <huynhqtienvtag@gmail.com>
2025-06-22 09:17:25 +0700

    make it buildable first

---
+++ update_versions.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+MODULE_DIRS=()
+
+# Function to recursively find valid pom.xml files
+find_modules() {
+  local dir="$1"
+  POM_FILE="$dir/pom.xml"
+  if [[ -f "$POM_FILE" && $(grep -q '<packaging>pom</packaging>' "$POM_FILE")$? -eq 0 ]]; then
+    MODULE_DIRS+=("$dir")
+    for subdir in "$dir"/*/; do
+      [[ -d "$subdir" ]] || continue
+      find_modules "$subdir"  # Recursively check for sub-modules
+    done
+  fi
+}
+
+# Start searching from current directory
+find_modules "$(pwd)"
+
+# Run Maven command for each valid module directory
+for dir in "${MODULE_DIRS[@]}"; do
+  echo "Updating versions for module in directory: $dir"
+  cd "$dir" || exit
+  mvn -N versions:update-child-modules -DgenerateBackupPoms=false
+done
