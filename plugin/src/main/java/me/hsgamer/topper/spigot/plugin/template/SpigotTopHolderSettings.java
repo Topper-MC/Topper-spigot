fd14ba030f2965fab3857f79fb24a58ac00d884a
hsgamer <huynhqtienvtag@gmail.com>
2026-02-12 23:12:39 +0700

    update topper usage

--- plugin/src/main/java/me/hsgamer/topper/spigot/plugin/template/SpigotTopHolderSettings.java
+++ plugin/src/main/java/me/hsgamer/topper/spigot/plugin/template/SpigotTopHolderSettings.java
@@ -5,86 +5,21 @@ import me.hsgamer.topper.agent.update.UpdateAgent;
 import me.hsgamer.topper.template.topplayernumber.holder.NumberTopHolder;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
-import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.*;
-import java.util.logging.Level;
 
-public class SpigotTopHolderSettings implements NumberTopHolder.Settings {
-    private final Map<String, Object> map;
+public class SpigotTopHolderSettings extends NumberTopHolder.MapSettings {
     private final List<String> ignorePermissions;
     private final List<String> resetPermissions;
 
     public SpigotTopHolderSettings(Map<String, Object> map) {
-        this.map = map;
+        super(map);
         ignorePermissions = CollectionUtils.createStringListFromObject(map.get("ignore-permission"), true);
         resetPermissions = CollectionUtils.createStringListFromObject(map.get("reset-permission"), true);
     }
 
-    @Override
-    public Double defaultValue() {
-        return Optional.ofNullable(map.get("default-value"))
-                .map(Object::toString)
-                .map(s -> {
-                    try {
-                        return Double.parseDouble(s);
-                    } catch (NumberFormatException e) {
-                        JavaPlugin.getProvidingPlugin(getClass()).getLogger().log(Level.WARNING, "Invalid default value: " + s + ". Fallback to null", e);
-                        return null;
-                    }
-                })
-                .orElse(null);
-    }
-
-    @Override
-    public String displayNullName() {
-        return Optional.ofNullable(map.get("null-name"))
-                .map(Object::toString)
-                .orElse("---");
-    }
-
-    @Override
-    public String displayNullUuid() {
-        return Optional.ofNullable(map.get("null-uuid"))
-                .map(Object::toString)
-                .orElse("---");
-    }
-
-    @Override
-    public String displayNullValue() {
-        return Optional.ofNullable(map.get("null-value"))
-                .map(Object::toString)
-                .orElse("---");
-    }
-
     public String defaultLine() {
         return Objects.toString(map.get("line"), null);
-    }
-
-    @Override
-    public boolean showErrors() {
-        return Optional.ofNullable(map.get("show-errors"))
-                .map(Object::toString)
-                .map(String::toLowerCase)
-                .map(Boolean::parseBoolean)
-                .orElse(false);
-    }
-
-    @Override
-    public boolean resetOnError() {
-        return Optional.ofNullable(map.get("reset-on-error"))
-                .map(Object::toString)
-                .map(String::toLowerCase)
-                .map(Boolean::parseBoolean)
-                .orElse(true);
-    }
-
-    @Override
-    public boolean reverse() {
-        return Optional.ofNullable(map.get("reverse"))
-                .map(String::valueOf)
-                .map(Boolean::parseBoolean)
-                .orElse(false);
     }
 
     @Override
@@ -103,14 +38,5 @@ public class SpigotTopHolderSettings implements NumberTopHolder.Settings {
             return UpdateAgent.FilterResult.SKIP;
         }
         return UpdateAgent.FilterResult.CONTINUE;
-    }
-
-    @Override
-    public Map<String, Object> valueProvider() {
-        return map;
-    }
-
-    public Map<String, Object> map() {
-        return map;
     }
 }
